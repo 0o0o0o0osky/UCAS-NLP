cc3d3133f7cabffb61bbbcb952cccb7cd9646984
0o0o0o0osky <90616158+0o0o0o0osky@users.noreply.github.com>
2023-03-10 20:06:50 +0800

    update
    update
    
    update

--- Assignment1/en-US/fetcher_en-US.py
+++ Assignment1/en-US/fetcher_en-US.py
@@ -1,1 +1,9 @@
 import newspaper
+
+# I'm not a bad guy. :-)
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+
+config = newspaper.Config()
+config.browser_user_agent = USER_AGENT
+config.fetch_images = False
+
